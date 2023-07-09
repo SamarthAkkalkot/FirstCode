91316c3ef0f40b5ee51900996c2333feca3a931b
SamarthAkkalkot <139058706+SamarthAkkalkot@users.noreply.github.com>
2023-07-09 19:46:16 +0530

    First Add

---
+++ A_Odd_Even.java
@@ -0,0 +1,16 @@
+package A_Numbers;
+
+public class A_Odd_Even {
+
+	public static void main(String[] args) {
+		int no=9;
+		
+		if(no%2==0) { //Modulo operator is used to determine remainder//
+			System.out.println("Is even number");
+		}
+		else {
+			System.out.println("Is odd number");
+		}
+	}
+
+}
